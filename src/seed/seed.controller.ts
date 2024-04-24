b3ff5d502dd62ad2c2c628612b1ed2d9476e1271
d-acetun <acetundiego@gmail.com>
2024-04-23 21:30:22 -0600

    fix: :adhesive_bandage: Move request to service

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,26 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
 import { SeedService } from './seed.service';
-import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Controller('seed')
 export class SeedController {
-  private readonly axios: AxiosInstance = axios;
   constructor(private readonly seedService: SeedService) {}
 
   @Get()
   async executeSeed() {
-    const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=10',
-    );
-
-    const { results } = data;
-    results.forEach(({ name, url }) => {
-      const segments = url.split('/');
-      const no: number = +segments[segments.length - 2];
-      console.log(`Name: ${name}, No: ${no}`);
-    });
-
-    return data.results;
+    return this.seedService.executeSeed();
   }
 }
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  executeSeed() {
-    return 'This action adds a new seed';
+  private readonly axios: AxiosInstance = axios;
+  async executeSeed() {
+    const { data } = await this.axios.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=10',
+    );
+
+    const { results } = data;
+    for (const { name, url } of results) {
+      const segments = url.split('/');
+      const no: number = +segments[segments.length - 2];
+      console.log(`Name: ${name}, No: ${no}`);
+    }
+
+    return data.results;
   }
 }
